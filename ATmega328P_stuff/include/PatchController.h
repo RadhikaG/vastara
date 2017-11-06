76a579255be10091d1b202f94b2b39507228eb66
RadhikaG <radhikaghosal@gmail.com>
2017-11-07 03:09:01 +0530

    atmega: midway commit, implemented most of the interface; todo: mix-up between uint8_t and bool

--- ATmega328P_stuff/include/PatchController.h
+++ ATmega328P_stuff/include/PatchController.h
@@ -1,18 +1,128 @@
 #ifndef PATCHCONTROLLER_H
 #define PATCHCONTROLLER_H
 
+#include <Wire.h>
+#include <inttypes.h>
+#define NOF_REQ_BYTES 4 ///< check PatchModule.h for size of `i2c_regs`
+#define MAX_DEVICES 5 ///< 128 is theoretical limit for I2C; this is for our system
+
+//---------- Global ISR Routines and Variables -------------//
+
+enum PatchStatus {
+    COMPLETELY_DISCONNECTED,
+    IFTTT_INVALID,
+    TANGIBLE_INVALID,
+    VALID_CONNECTED
+}; 
+
+enum PatchMode {
+    TANGIBLE,
+    IFTTT
+};
+
+enum InputLogic {
+    OR,
+    AND
+};
+
+volatile PatchStatus masterStatus; ///< value depends on status of central patch
+// Sets the delay for the status LED blinking frequency, whenever the device
+// status is changed.
+void setStatusLEDDelay();
+// Just makes the LED blink according to the delay set above.
+void statusLEDISR();
+
+// Modified by an ISR set for an onboard slider switch.
+// Controls if system is configured tangibly or via IFTTT.
+volatile PatchMode masterMode; 
+// And the slider switch ISR for modifying the above variable.
+void modeSwitchISR();
+
+// The below is only valid for tangible mode.
+// Controls whether the inputs are combined using OR or AND.
+volatile InputLogic inputLogic;
+// And the slider switch ISR for modifying the above variable.
+void logicSwitchISR();
+
+SoftwareSerial bluetoothSerial(ssRX, ssTX);
+
+//-----------------------------------------------------------------------//
+
+/* \brief Central Patch <-> Patch Module <-> Sensor/Device
+ *
+ * uC: ATmega328P
+ */
 class PatchController {
-    // Central Patch
-    // Central Patch <-> Patch Module <-> Sensor/Device
-    // Mux between the central patch and the set of patch
-    // modules because all the patch modules have the same
-    // I2C address.
-    // uC: ATmega328P
 
 public:
+    // Analog-in 4 and 5 reserved for SDA and SCL for I2C respectively.
+
+    // Digital pins 10 and 11 for HC-05 bluetooth module. We don't use the default
+    // built-in UART interface for Bluetooth, instead we're using SoftwareSerial 
+    // on pins 10 and 11, to use the default UART for PC debugging.
+    static const uint8_t ssRX = 10;
+    static const uint8_t ssTX = 11;
+
+    // Digital output pin 13 for status LED.
+    static const uint8_t ledPin = 13;
+
+    // TODO: change switch pins depending on interrupts
+    // Digital input pin 4 for slider switch to switch between tangible and IFTTT
+    // mode.
+    static const uint8_t modeSwitchPin = 4;
+
+    // Digital input pin 5 for slider switch to switch between AND and OR for 
+    // input combining.
+    static const uint8_t logicSwitchPin = 5;
+
+    //--------- Current state of the system ----------------//
+
+    uint8_t nof_current_devices;
+    uint8_t currentDevices[MAX_DEVICES]; ///< stores addresses of the connected patches
+
+    uint8_t nof_current_inputs;
+    uint8_t currentInputDevices[MAX_DEVICES];
+
+    uint8_t nof_current_outputs;
+    uint8_t currentOutputDevices[MAX_DEVICES];
+
+    // Bitvector to store active HIGH or active LOW of input/output addresses
+    uint8_t currentActiveState[16]; ///< for a maximum of 128 addresses
+
+    //-------------- Intended user config ----------------//
+    // May not be the same as the current state of the system!
+    // (which is why we have the validateIfttt() and the validateTangible()
+    // functions to check if the current state is the same as the user config)
+
+    uint8_t nof_config_inputs;
+    uint8_t configInputDevices[MAX_DEVICES];
+    uint8_t nof_config_outputs;
+    uint8_t configOutputDevices[MAX_DEVICES];
+    uint8_t configActiveState[16];
+
+    //-----------------------------------------------------------------------//
 
     PatchController();
 
+    // Takes in integer stream from mobile app via Bluetooth, and configures
+    // the application logic accordingly.
+    void iftttConfig();
+    // Validates connected devices for operation, then sets logic depending on
+    // tangible or IFTTT.
+    // Called by setStatus.
+    void validateIfttt();
+    void validateTangible();
+    void setStatus();
+
+    // I2C-specific functions for dumping data to and fro.
+    void sendDataTo(uint8_t slaveAddr, uint8_t data);
+    void receiveDataFrom(uint8_t slaveAddr);
+
+    // Finds all the I2C devices on the bus. 
+    // Invokes validateIfttt or validateTangible after scanning done.
+    void scanForI2CDevices();
+    // Application logic loop.
+    void pollDevices();
 }
 
 #endif
--- ATmega328P_stuff/lib/PatchController.cpp
+++ ATmega328P_stuff/lib/PatchController.cpp
@@ -1,6 +1,156 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include "PatchController.h"
 
 PatchController::PatchController() {
+    // Setting global variables
+    nof_current_devices = 0;
+    nof_current_inputs = 0;
+    nof_current_outputs = 0;
 
+    masterMode = digitalRead(modeSwitchPin);
+    inputLogic = digitalRead(logicSwitchPin);
+    masterStatus = updateMasterStatus();
+
+    // Initializing I2C master.
+    Wire.begin();
+    // Start Serial for output.
+    Serial.begin(9600);
+
+    // Initializing Bluetooth.
+    bluetoothSerial.begin(9600);
+}
+
+void PatchController::iftttConfig() {
+    // We can only configure the system when the central patch is disconnected
+    // from all mini-patches.
+    if (masterStatus == COMPLETELY_DISCONNECTED && isIfttt) {
+        if (bluetoothSerial.available()) {
+            // TODO: fill in bluetooth API details for config
+        }
+    }
+}
+
+int PatchController::validateIfttt() {
+    // We check if current connected state of system is the same as intended user config
+
+}
+
+int PatchController::validateTangible() {
+    // Here, we simply check if the connected state adheres to certain rules, namely:
+    // * At least one input patch
+    // * At least one output patch
+    // And we accordingly set the user config variables, most notably,
+    // the activeStates of the devices, since we get those via the slider switch 
+    // inputs present on the mini-patches.
+
+}
+
+void PatchController::updateMasterStatus() {
+    if (masterMode == TANGIBLE) {
+        if (validateTangible() == 1) {
+            masterStatus = VALID_CONNECTED;
+        }
+        else if (validateTangible() == 0) {
+            masterStatus = COMPLETELY_DISCONNECTED;
+        }
+        else if (validateTangible() == -1) {
+            masterStatus = TANGIBLE_INVALID;
+        }
+    }
+    else if (masterMode == IFTTT) {
+        if (validateIfttt() == 1) {
+            masterStatus = VALID_CONNECTED;
+        }
+        else if (validateIfttt() == 0) {
+            masterStatus = COMPLETELY_DISCONNECTED;
+        }
+        else if (validateIfttt() == -1) {
+            masterStatus = IFTTT_INVALID;
+        }
+    }
+}
+
+void PatchController::sendDataTo(uint8_t slaveAddr, uint8_t data) {
+    // Ref: https://www.arduino.cc/en/Tutorial/MasterWriter
+    Wire.beginTransmission(slaveAddr);
+    Wire.write(data); // TODO: fill in data according to slave
+    Wire.endTransmission();
+}
+
+void PatchController::receiveDataFrom(uint8_t slaveAddr) {
+    // Ref: https://www.arduino.cc/en/Tutorial/MasterReader
+    
+    Wire.request(slaveAddr, NOF_REQ_BYTES);
+
+    while (Wire.available()) {
+        uint8_t data = Wire.read();
+    }
+}
+
+void PatchController::scanForI2CDevices() {
+    // Ref: https://playground.arduino.cc/Main/I2cScanner
+    uint8_t error, addr;
+
+    for (addr = 0; addr < 127; addr++) {
+        Wire.beginTransmission(addr);
+        error = Wire.endTransmission();
+
+        // I2C patch found
+        if (error == 0) {
+            
+        }
+        else {
+
+        }
+    }
+}
+
+void PatchController::getActiveState(uint8_t slaveAddr) {
+    // TODO: bit magic on deviceActiveState
+}
+
+void PatchController::setActiveState(uint8_t slaveAddr, bool state) {
+    // TODO: bit magic on deviceActiveState
+}
+
+void PatchController::pollDevices() {
+    // Run in loop() of Arduino sketch
+    // see enum `PatchStatus` if comparison not clear
+    if (masterStatus < VALID_CONNECTED) {
+        scanForI2CDevices();
+        updateMasterStatus();
+        delay(500); // we scan for new connected patches every 500 ms
+    }
+    // System is valid and configured; now it runs according to user-defined logic
+    else {
+        bool outputState;
+
+        uint8_t i;
+        for (i = 0; i < nof_current_inputs; i++) {
+            uint8_t slaveAddr = currentInputDevices[i];
+            uint8_t rawInputData = receiveDataFrom(slaveAddr);
+            uint8_t activateOutputState = ~(rawInputData ^ getActiveState(slaveAddr));
+
+            // We perform no logical operations on the first input checked
+            if (i == 0) {
+                outputState = activateOutputState;
+            }
+            else {
+                if (inputLogic == OR) {
+                    outputState |= activateOutputState;
+                }
+                else if (inputLogic == AND) {
+                    outputState &= activateOutputState;
+                }
+            }
+        }
+
+        for (i = 0; i < nof_current_outputs; i++) {
+            uint8_t slaveAddr = currentOutputDevices[i];
+            sendDataTo(slaveAddr, outputState);
+        }
+
+        delay(100);
+    }
 }
